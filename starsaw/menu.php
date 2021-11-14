b17ebe437e9caedfe9477c4d4917b73ba17b47a3
Luca <58145073+luca959@users.noreply.github.com>
2021-11-14 12:09:36 +0100

    Add files via upload
    modificato menu e footer in php

---
+++ starsaw/menu.php
@@ -0,0 +1,30 @@
+<?php
+
+echo '
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonshop: Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <nav>
+  <div id="logo">
+      <img src="img/logo.jpg" alt="">
+  </div>
+
+  <div id="buttons">
+      <a href="#">Carrello</a>
+      <a href="#">Accedi</a>
+      <a href="#">Registrati</a>
+  </div>
+</nav>
+';
+?>
