3609453b0a82092bc1edda8b3b1a72e924fe6e8b
Davide Scarrà <73951384+LoScarro@users.noreply.github.com>
2021-11-17 19:22:35 +0100

    Add files via upload
    varie migliorie al css della pagina di login e registration

--- Progetto/menu.php
+++ Progetto/menu.php
@@ -6,6 +6,8 @@ echo '
   </div>
 
   <div id="buttons">
+
+      <a href="index.php">Home</a>
       <a href="#">Carrello</a>
       <a href="login.php">Accedi</a>
       <a href="registration.php">Registrati</a>
