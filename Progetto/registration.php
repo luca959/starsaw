4e7bdbb815785e0fd944091be7e0aa18c9cde3e4
Luca <58145073+luca959@users.noreply.github.com>
2021-11-19 18:00:08 +0100

    Add files via upload
    aggiunto attributo required a login e registration

---
+++ Progetto/registration.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>Registrazione</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <?php
+    include 'menu.php'
+    ?>
+
+<div class="mydiv"> 
+   <div> 
+    <form id="myform" action="registrationProcess.php" method="POST"> 
+
+    <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="text" class="inputHover" name="nome" placeholder="Nome" required><br>
+
+    <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="text" class="inputHover" name="cognome" placeholder="Cognome" required><br>
+
+    <i class="fa fa-envelope" style="font-size:9px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="email" id="" class="inputHover"  name="email" placeholder="E-mail" required><br>
+
+    <i class="fa fa-unlock-alt" style="font-size:14px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="password" class="inputHover" name="password" placeholder="Password" required><br>
+
+    <i class="fa fa-unlock-alt" style="font-size:14px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="password" class="inputHover"  name="conferma" placeholder="Conferma password" required><br>
+
+    <input type="submit" value="Invia">
+
+   </form>
+  </div>
+
+</div>
+
+<?php
+    include 'footer.php'
+?>
+</body>
+</html>
+
---
+++ Progetto/registrationProcess.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+	<meta charset="UTF-8">
+    <title>Sign-up</title>
+</head>
+
+<body>
+
+<?php
+$filename = $_SERVER['DOCUMENT_ROOT'] . "/../private/mydata.txt"; // vado a creare il database al di fuori della doc root
+$database = fopen($filename, "a"); //con a apro il file in append mode
+
+    if (empty($_POST['firstname'])||empty($_POST['lastname'])||empty($_POST['pass']) ||empty($_POST['confirm']) || empty($_POST['email'])) //controllo se il campo è vuoto 
+        die('<h1>Campi Mancanti!</h1>');
+
+
+
+
+    include 'connection.php';             // mi connetto al database
+    //include 'allusers.php'; 
+    $dati[0] = mysqli_real_escape_string($con, $_POST['firstname']); //$dati[0] contiene il nome
+    $dati[1] = mysqli_real_escape_string($con, $_POST['lastname']); //$dati[1] contiene il cognome
+    $dati[2] = $_POST['pass']; //$dati[2] contiene la password
+    $dati[3] = $_POST['confirm']; //$dati[3] contiene la conferma della password
+    $dati[4] = mysqli_real_escape_string($con, $_POST['email']); //$dati[4] contiene la mail
+    
+    if ($dati[2] != $dati[3]) {
+         die("<h1>Le password non corrispondono</h1>");
+    }
+
+    /*-----------------------------------------------------------------INIZIO INTERAZIONE COL DATABASE-----------------------------------------------------------------*/
+    
+    $dati[2]=password_hash($dati[2], PASSWORD_DEFAULT);      //cripto la password
+    
+    $insert_query = "INSERT INTO utenti (id, firstname, lastname, email, pass) 
+                    VALUES (NULL, '".$dati[0]."', '".$dati[1]."', '".$dati[4]."', '".$dati[2]."');";        //query per aggiungere l'utente al db
+    
+    $select_query = "SELECT * FROM utenti WHERE email='".$dati[4]."'";      //query per vedere se c'è già un utente con quella mail
+    
+    $res=mysqli_query($con, $select_query);     //eseguo la query per vedere se c'è già un utente con quella mail
+    
+    if(mysqli_num_rows($res)==0){       //se non c'è lo aggiungo (eseguendo la quesry di inserimento)
+        echo "<h1>Benvenuto nel club ".$dati[0]." </h1>";
+        $res=mysqli_query($con, $insert_query);
+    }
+    else{
+        die ("<h1> E' già presente un utente con questa mail</h1>");       //altrimenti segnalo l'errore
+    }
+
+    /*-----------------------------------------------------------------FINE INTERAZIONE COL DATABASE-----------------------------------------------------------------*/
+
+    ?>
+
+</body>
+</html>
