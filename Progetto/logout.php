67f9738c018f8ef4121e7234ba55b3306328dcfe
Davide Scarrà <73951384+LoScarro@users.noreply.github.com>
2021-11-23 20:51:39 +0100

    Add files via upload

--- Progetto/logout.php
+++ Progetto/logout.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
-<html lang="it">
+<html lang = "it">
 <head>
-	<meta charset="UTF-8">
-    <title>Logout</title>
+    <title>Accedi</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-
+   <?php
+include 'menu.php';
+?>
+<div>
+<h1>Grazie per aver visitato Bonshop</h1>
+</div>
 <?php
-session_start();
+
+include 'footer.php';
 unset($_SESSION['id']);
 $_SESSION = array();
 session_destroy();
-setcookie(session_name(), '', time() - 42000);
-header('Location: index.php');
-exit;
-?>
+//setcookie(session_name(), '', time() - 42000);
 
+//header("Refresh:5; url=index.php");
+
+?>
 </body>
 </html>
