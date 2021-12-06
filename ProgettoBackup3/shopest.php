7d6a13409f9d70dfb974d915db89ed8981f7161a
Luca <58145073+luca959@users.noreply.github.com>
2021-12-06 16:00:53 +0100

    Add files via upload

---
+++ ProgettoBackup3/shopest.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonshop: Bonsai da Esterno</title>
+    <link rel="icon" type="image/x-icon" href="/Progetto/img/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+<?php
+include "menu.php";
+?>
+  <div class="mydiv">
+
+
+    <div>
+      <div class="product">
+          <img class="productImage" src="img/ficus.png" alt="Bonsai di Ficus">
+          <p class="productDescription"><b>Ficus</b> <br>
+          Il genere Ficus appartiene alla famiglia delle piante di gelso. </p>
+     </div>
+     <div class="button1">
+        <p style="text-align: center;" >Aggiungi al Carello</p>
+     </div>
+
+    </div>
+
+
+
+  <div>
+      <div class="product">
+          <img class="productImage" src="img/sageretia.png" alt="Bonsai di Sageretia">
+          <p class="productDescription"><b>Sageretia</b> <br>
+          La Sageretia è una pianta sempreverde tropicale, originaria della Cina. </p>
+      </div>
+      <div class="button1">
+        <p style="text-align: center;" >Aggiungi al Carello</p>
+     </div>
+  </div>
+
+  <div>
+      <div class="product">
+
+      </div>
+      <div class="button1">
+        <p style="text-align: center;" >Aggiungi al Carello</p>
+     </div>
+</div>
+
+
+<div>
+      <div class="product">
+
+      </div>
+      <div class="button1">
+        <p style="text-align: center;" >Aggiungi al Carello</p>
+     </div>
+</div>
+
+<div>
+      <div class="product">
+
+      </div>
+      <div class="button1">
+        <p style="text-align: center;" >Aggiungi al Carello</p>
+     </div>
+</div>
+
+
+  </div>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
