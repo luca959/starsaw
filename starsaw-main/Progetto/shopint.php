5928a9a8b1545726a49da6f81c15604ceb6297cf
Luca <58145073+luca959@users.noreply.github.com>
2021-12-22 18:30:37 +0100

    Add files via upload

---
+++ starsaw-main/Progetto/shopint.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonshop: Bonsai da Interno</title>
+    <link rel="icon" type="image/x-icon" href="/Progetto/img/home/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+<div class="mydiv">
+<?php
+include "menu.php";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/../private/connection.php';
+
+$select_query = "SELECT * FROM items WHERE esposizione = '0'";
+$res = mysqli_query($con, $select_query);
+include "products.php";
+?>
+</div>
+<?php
+include "footer.php";
+?>
+</body>
+</html>
